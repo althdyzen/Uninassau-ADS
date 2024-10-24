5ffc137f7daf9caffbce77e57df19dfc2e72097e
althdyzen <zenntemdinheiro@gmail.com>
2024-10-24 09:53:25 -0300

    Update

--- Programação Orientada a Objetos/Aula 24-10-24/Main.java
+++ Programação Orientada a Objetos/Aula 24-10-24/Main.java
@@ -1,45 +1,29 @@
-class Main {
-    // 1 - Metodo
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        Aluno[] notas = new Aluno[20];
+class Main {
+    public static void main(String[] args) {
+        double notas[] = new double[5];
+        Scanner s = new Scanner(System.in);
 
         for (int i = 0; i < notas.length; i++) {
-            notas[i] = new Aluno(Math.random() * 10);
+            System.out.println("Digite a nota: ");
 
-            if (notas[i].getNota() > 7.5) {
-                System.out.printf("Nota: %.1f%n", notas[i].getNota());
+            try {
+                notas[i] = s.nextDouble();
+            } catch (java.util.InputMismatchException e) {
+                i--;
+                System.out.println("A nota precisa ser um número!");
+                s.next();
+            }
+        }
+
+        System.out.println("Notas > 7.5");
+        for (double nota : notas) {
+            if (nota > 7.5) {
+                System.out.println(nota);
             }
         }
     }
-
-    // 2 - Metodo
-    // public static void main(String[] args) {
-
-    // double[] notas = new double[20];
-
-    // for (int i = 0; i < notas.length; i++) {
-    // notas[i] = Math.random() * 10;
-    // // System.out.println(notas[i]);
-
-    // if (notas[i] > 7.5) {
-    // System.out.printf("Nota: %.1f%n", notas[i]);
-    // }
-    // }
-    // }
-
-    // 3 - Metodo
-    // public static void main(String[] args) {
-    // double[] notas = { 9.2, 7.5, 2.7, 5.5, 8.8, 1.9, 4.4, 8.2, 5.6, 3.9, 8.0,
-    // 4.3, 6.1, 4.5, 3.3, 7.1, 4.4, 6.7,
-    // 5.8, 6.5 };
-
-    // for (double nota : notas) {
-    // if (nota > 7.5) {
-    // System.out.println(notas[i]);
-    // }
-    // }
-    // }
 }
 
 class Aluno {
